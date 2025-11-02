53805df5161fa58e8c0e119566c8d85ea5ece697
ItzNilay <97908176+ItzNilay@users.noreply.github.com>
2025-11-02 23:48:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
+ document.addEventListener('DOMContentLoaded', () => {
     // --- DOM Elements ---
     const monthDisplay = document.getElementById('month-display');
     const calendarGrid = document.getElementById('calendar-grid');
